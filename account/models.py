2b0584f4063839e3b50ad6bf08d8315fdf01c9b4
asis <errorak143@gmail.com>
2018-10-30 15:17:27 +0545

    aashish

--- account/models.py
+++ account/models.py
@@ -2,6 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+class User(models.Model):
+	username = models.CharField(max_length=250)
+	password = models.CharField(max_length= 250)
+	email =models.CharField(max_length=270)
+
+class offer(models.Model):
+	offer_Name = models.CharField(max_length=250)
+	Description = models.CharField(max_length= 400)
+	item = models.CharField(max_length=450)
+
+class Partner(models.Model):
+	Partener_company = models.CharField(max_length=250)
+	Associted_person = models.CharField(max_length= 250)
+	email =models.CharField(max_length=270)
+	Phone_number =models.CharField(max_length=270)
 
 class DiscountAmount(models.Model):
     # silver=models.CharField(max_length=250)
@@ -25,27 +40,9 @@ class Promotions(models.Model):
     image= models.ImageField(upload_to='profile_image', blank=False, default='')
     description = models.TextField(max_length=1000, blank=False, default='')
 
-class User(models.Model):
-	username = models.CharField(max_length=250)
-	password = models.CharField(max_length= 250)
-	email =models.CharField(max_length=270)
-
-class offers(models.Model):
-	offer_Name = models.CharField(max_length=250)
-	Description = models.CharField(max_length= 400)
-	item = models.CharField(max_length=450)
-
-class Partner(models.Model):
-	Partener_company = models.CharField(max_length=250)
-	Associted_person = models.CharField(max_length= 250)
-	email =models.CharField(max_length=270)
-	Phone_number =models.CharField(max_length=270)
-
 class Trasaction(models.Model):
     CustomerInfo = models.ForeignKey(CustomerInfo, on_delete=models.CASCADE , null=True)
     Partner = models.ForeignKey(Partner,on_delete=models.CASCADE)
     Discount = models.CharField(max_length=100)
     Total = models.CharField(max_length=100)
-
-
 
