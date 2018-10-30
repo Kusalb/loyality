b89ce5f2b70faaf7d68bef494c1d7ba4f3861290
asis <errorak143@gmail.com>
2018-10-30 15:18:32 +0545

    Final model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User
+
+from .models import offer
+
+from .models import Partner
+
+from .models import DiscountAmount
+
+from .models import CustomerInfo
+
+from .models import Promotions
+
+from .models import Trasaction
+
+
+admin.site.register(User)
+admin.site.register(offer)
+admin.site.register(Partner)
+admin.site.register(DiscountAmount)
+admin.site.register(CustomerInfo)
+admin.site.register(Promotions)
+admin.site.register(Trasaction)
+
