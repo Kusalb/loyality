c7c2b2407795f31e2c111ebd5ccef53f9ccde8dc
asis <errorak143@gmail.com>
2018-11-06 13:13:02 +0545

    Final model

--- account/models.py
+++ account/models.py
@@ -5,21 +5,23 @@ from django.db import models
 class User(models.Model):
 	username = models.CharField(max_length=250)
 	password = models.CharField(max_length= 250)
-	email =models.CharField(max_length=270)
-
-
-class offer(models.Model):
-	offer_Name = models.CharField(max_length=250)
-	Description = models.CharField(max_length= 400)
-
+	email = models.EmailField()
 
 class Partner(models.Model):
 	Partener_company = models.CharField(max_length=250)
 	Associted_person = models.CharField(max_length= 250)
-	email =models.CharField(max_length=270)
+	email = models.EmailField()
 	Phone_number =models.CharField(max_length=270)
 
+class offer(models.Model):
+
+    Partner = models.ForeignKey(Partner, on_delete=models.CASCADE, null=True)
+    offer_Name = models.CharField(max_length=250)
+    Description = models.CharField(max_length= 400)
+
+
 class Discount(models.Model):
+    Partner = models.ForeignKey(Partner, on_delete=models.CASCADE)
     silver=models.CharField(max_length=250, default='')
     gold=models.CharField(max_length=250 , default='')
     platinum=models.CharField(max_length=250,default='')
@@ -30,13 +32,12 @@ class Discount(models.Model):
 
 class Customer(models.Model):
     chasis_no = models.CharField(max_length=200)
-    c_id = models.CharField(max_length=100)
     password= models.CharField(max_length=100)
     first_name = models.CharField(max_length=50, default='')
     last_name = models.CharField(max_length=50, default='')
 
 class Promotion(models.Model):
-
+    Partner = models.ForeignKey(Partner, on_delete=models.CASCADE , null=True)
     promotion_type = models.CharField(max_length=50, default='')
     image= models.ImageField(upload_to='profile_image', default='')
     description = models.TextField(max_length=1000, default='')
